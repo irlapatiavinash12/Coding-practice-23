c2addfc53df9cea8280d09a71f4d4f0a406d3134
irlapatiavinash12 <irlapati.avinash12@gmail.com>
2024-07-02 05:28:11 +0000

    routing

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+import UserInfo from '../UserInfo'
+
+import BlogList from '../BlogList'
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <UserInfo />
+      <BlogList />
+    </div>
+  )
+}
+
+export default Home
